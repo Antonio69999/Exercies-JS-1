6efb944c60597c4e0ad84409cb418f465242c0ca
Antonio69999 <antoine.chabeauti@gmail.com>
2023-06-20 13:47:28 +0200

    exercie 1 to 10

--- Exercice_1/exercice1.js
+++
@@ -1,7 +0,0 @@
-// Exercice 1 :
-// Définir une variable et l'afficher dans une boîte de dialogue.
-
-let my_variable;
-my_variable = 1;
-
-alert (my_variable);
--- Exerice_2/exrecice_2.js
+++
@@ -1,9 +0,0 @@
-let lastName;
-let firstName;
-let city;
-
-lastName = "Chabeauti";    
-firstName = "Antoine";
-city = "Sainté";
-
-alert("Nom : " + lastName +"\n" + "Prénom : "+ firstName +"\n" + "Ville : " + city)
